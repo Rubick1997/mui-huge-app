a936ec77b8f00347ba33118990b7758116b6fff4
Rustam <rustam905@gmail.com>
2021-05-12 17:21:11 -0700

    Icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Typography, Button, Container } from "@material-ui/core";
+import { AcUnitOutlined,Send,KeyboardArrowRight } from "@material-ui/icons";
 
 export default function Create() {
-  return (
-    <div>
-      Create page
-    </div>
-  )
+	return (
+		<Container>
+			<Typography
+				variant='h6'
+				component='h2'
+				gutterBottom
+				color='textSecondary'>
+				Create a New Note
+			</Typography>
+
+			<Button
+				type='submit'
+				color='secondary'
+				variant='contained'
+				disableElevation
+        endIcon={<KeyboardArrowRight/>}
+        >
+				Submit
+			</Button>
+			{/* <br />
+			<AcUnitOutlined />
+			<AcUnitOutlined color='secondary' fontSize='large' />
+			<AcUnitOutlined color='secondary' fontSize='small' />
+			<AcUnitOutlined color='action' fontSize='small' />
+			<AcUnitOutlined color='error' fontSize='small' />
+			<AcUnitOutlined color='disabled' fontSize='small' /> */}
+		</Container>
+	);
 }
