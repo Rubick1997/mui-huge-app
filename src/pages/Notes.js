ed491ad5c5ae0b61c44404dd7d732389110cea8e
Rustam <rustam905@gmail.com>
2021-05-13 22:04:13 -0700

    masonry grid

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,25 +1,35 @@
-import React, { useEffect} from "react";
-import { Container, Grid } from "@material-ui/core";
+import React, { useEffect } from "react";
+import { Container } from "@material-ui/core";
 import NoteCard from "../components/NodeCard";
 import useHttp from "../hooks/useHttp";
+import Masonry from "react-masonry-css";
 
 export default function Notes() {
-  const { state, deleteHandler,sendRequest } = useHttp();
+	const { state, deleteHandler, sendRequest } = useHttp();
 
 	useEffect(() => {
 		sendRequest();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	const breakpoints = {
+		default: 3,
+		1100: 2,
+		700: 1,
+	};
+
 	return (
 		<Container>
-			<Grid container spacing={3}>
+			<Masonry
+				breakpointCols={breakpoints}
+				className='my-masonry-grid'
+				columnClassName='my-masonry-grid_column'>
 				{state.map((note) => (
-					<Grid item key={note.id} xs={12} md={6} lg={4}>
+					<div item key={note.id}>
 						<NoteCard note={note} handleDelete={deleteHandler} />
-					</Grid>
+					</div>
 				))}
-			</Grid>
+			</Masonry>
 		</Container>
 	);
 }
